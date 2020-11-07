003387f3537ec450c228b0b2b8c6a82d0733484b
amer519 <afahmy519@gmail.com>
2020-11-07 21:20:58 +0200

    logic for hero, added home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Button } from './Button'
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './Hero.css';
+
 
 
 function Hero() {
@@ -15,6 +18,13 @@ function Hero() {
                 buttonSize='btn--large'
                 >
                     GET STARTED
+                </Button>
+                <Button 
+                className='btns' 
+                buttonStyle='btn--primary' 
+                buttonSize='btn--large'
+                >
+                    WATCH TRAILER <i className='far fa-play-circle' />
                 </Button>
             </div>
             
