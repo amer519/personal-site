5fa56c8b901a4d413c1a0ce9d3b298f72939600e
amer519 <afahmy519@gmail.com>
2020-11-15 20:01:03 +0200

    added cards css and props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
     return (
         <div className='cards'>
             <h1>Sample text</h1>
-            <div className='cards_container'>
-                <div className='cards_wrapper'>
-                    <ul className='cards_items'>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
                         <CardItem 
                             src='images/img-9.jpg'
-                            text='explore'
+                            text='explore the waterfall'
+                            label='Adventure'
+                            path='/services'
                         />
                     </ul>
                 </div>
