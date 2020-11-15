043be393855c5fca8ccedd4d2e9ef2d0177b9a49
amer519 <afahmy519@gmail.com>
2020-11-15 20:26:55 +0200

    added cards, css and props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,6 +15,32 @@ function Cards() {
                             label='Adventure'
                             path='/services'
                         />
+                        <CardItem 
+                            src='images/img-2.jpg'
+                            text='Travel the Islands'
+                            label='Travel'
+                            path='/services'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem 
+                            src='images/img-9.jpg'
+                            text='explore the waterfall'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem 
+                            src='images/img-2.jpg'
+                            text='Travel the Islands'
+                            label='Travel'
+                            path='/services'
+                        />
+                        <CardItem 
+                            src='images/img-9.jpg'
+                            text='explore the waterfall'
+                            label='Adventure'
+                            path='/services'
+                        />
                     </ul>
                 </div>
             </div>
