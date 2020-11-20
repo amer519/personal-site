f9bd861eeff395ce7c79385fdb31cc17296a3198
amer519 <afahmy519@gmail.com>
2020-11-20 16:30:02 +0200

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section
+                className='footer-subscription'>
+                    <p className='footer-subscription-heading'>
+                        Join
+                    </p>
+                    <p className='footer-subscription-text'>
+                        sample text
+                    </p>
+                    <div className='input-areas'>
+                        <form>
+
+                        </form>
+                    </div>
+            </section>
+        </div>
+    )
+}
+
+export default Footer
