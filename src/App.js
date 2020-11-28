0238b245db8040ce960329eddb3c4e0b50a23f6b
amer519 <afahmy519@gmail.com>
2020-11-28 14:10:19 +0200

    contact  page added signup feature deleted

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home'
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import Contact from './components/pages/Contact';
+// import SignUp from './components/pages/SignUp';
 import About from './components/pages/About';
 import Footer from './components/Footer'
 
@@ -17,8 +17,8 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' exact component={About} />
-          <Route path='/products' exact component={Products} />
-          <Route path='/sign-up' exact component={SignUp} />
+          <Route path='/contact' exact component={Contact} />
+          {/* <Route path='/sign-up' exact component={SignUp} /> */}
         </Switch>
       </Router>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,18 +49,18 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                                Products
+                            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                                Contact 
                             </Link>
                         </li>
-                        <li>
+                        {/* <li>
                             <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                                 Sign Up
                             </Link>
                             
-                        </li>
+                        </li> */}
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+                    {/* {button && <Button buttonStyle='btn--outline'>Sign Up</Button>} */}
                 </div>
             </nav>
         </>
