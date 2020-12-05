b86a4a20c1345ce2091aefeb37ef6302d7420d0c
amer519 <afahmy519@gmail.com>
2020-12-05 22:43:00 +0200

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,6 +8,15 @@ export default function About() {
     return (
         <div className='service-pic'>
             <h1>About</h1>
+            <p>Hello, my name is Amer. I'm a full stack engineer with experience in JavaScript, React, Redux, Ruby, Ruby on Rails, ActiveRecord, HTML, CSS with a background in computer science.</p>
+
+            <p>I'm always trying my hardest to get better at the things i've learned as well as learn new things on the way!</p>
+
+            <p>I'm pretty new to the coding world but I fell in love with it quickly.</p>
+
+            <p>I'm fortunate to be a Flation School Alumni and in a short 4 months learned a handful of languages and frameworks.</p>
+
+            <p>If you have any question or would like to contact me please dont hesitate and head over to my contact page!!</p>
             {/* <img src='images/img-9.jpg'  /> */}
             <Footer />
         </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,15 +6,7 @@ import Footer from '../Footer'
 export default function Contact() {
     return (
     <div>
-    <h1 className='Contact'><p>Hello, my name is Amer. I'm a full stack engineer with experience in JavaScript, React, Redux, Ruby, Ruby on Rails, ActiveRecord, HTML, CSS with a background in computer science.</p>
-
-    <p>I'm always trying my hardest to get better at the things i've learned as well as learn new things on the way!</p>
-
-    <p>I'm pretty new to the coding world but I fell in love with it quickly.</p>
-
-    <p>I'm fortunate to be a Flation School Alumni and in a short 4 months learned a handful of languages and frameworks.</p>
-
-    <p>If you have any question or would like to contact me please dont hesitate and head over to my contact page!!</p></h1>
+    <h1 className='Contact'></h1>
 
     <Footer />
     </div>
